6449da3566ae548b58d7fbb9293611dbcab75b0f
Thiago <ps-thiago@hotmail.com>
2022-10-02 21:02:51 -0300

    Feat: Home Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,29 @@
-import { styled } from '../styles'
+import { HomeContainer, Product } from '../styles/pages/home'
+import Image from 'next/future/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span> Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt={''} width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90 </span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt={''} width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90 </span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
