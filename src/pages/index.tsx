6eb769e16ff8e7b07e168016d04d7de6016bfd68
Thiago <ps-thiago@hotmail.com>
2022-10-03 15:24:04 -0300

    Fix: Change ServerSide to Static, and del assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,9 @@ import { HomeContainer, Product } from '../styles/pages/home'
 import Image from 'next/future/image'
 import { useKeenSlider } from 'keen-slider/react'
 
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '../lib/stripe'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
 
 interface HomeProps {
@@ -46,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -64,6 +60,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+
+    revalidate: 60 * 60 * 2
   }
 }
