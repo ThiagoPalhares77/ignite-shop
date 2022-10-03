d4b21489653d961d55a6358d15ae096ff1262d0e
Thiago <ps-thiago@hotmail.com>
2022-10-03 16:51:47 -0300

    Feat: Product Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { HomeContainer, Product } from '../styles/pages/home'
 import Image from 'next/future/image'
 import { useKeenSlider } from 'keen-slider/react'
+import Link from 'next/link'
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '../lib/stripe'
@@ -28,14 +29,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} alt={''} width={520} height={480} />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt={''} width={520} height={480} />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,28 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
-  return <h1>Product:{JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed iste
+          quaerat quo velit cum? Hic omnis cupiditate explicabo asperiores nam
+          ex, eius cum. Amet facere ab rerum aut odio placeat.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
